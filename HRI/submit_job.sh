f0cd5c079cfac9c5ea5138bb13df94fe09096032
Penguin-rgb-dev <divesh20243414@students.iisertirupati.ac.in>
2026-08-27 14:44:44 +0530

    modified such that the output files names have jobid in them.

--- HRI/submit_job.sh
+++ HRI/submit_job.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=32
 #PBS -q workq
 #PBS -N cvh_test
-#PBS -e job.err
-#PBS -o job.out
+#PBS -e job_${PBS_JOBID}.err
+#PBS -o job_${PBS_JOBID}.out
 
 # Move to the directory where the job was submitted
 cd $PBS_O_WORKDIR
@@ -28,19 +28,25 @@ export JOBLIB_CPU_COUNT=$CORES
 export OMP_NUM_THREADS=$CORES
 export MKL_NUM_THREADS=$CORES
 
-echo "Job started on $(hostname) at $(date)" > log.txt
-echo "Allocated Cores: $CORES" >> log.txt
-echo "----------------------------------------" >> log.txt
+# Clean job ID (optional: removes the hostname suffix, turning '12345.master1' into '12345')
+JOB_ID=$(echo $PBS_JOBID | cut -d'.' -f1)
+
+# Log file with Job ID prefix (e.g., 12345_log.txt)
+LOGFILE="${JOB_ID}_log.txt"
+
+echo "Job started on $(hostname) at $(date)" > "$LOGFILE"
+echo "Allocated Cores: $CORES" >> "$LOGFILE"
+echo "----------------------------------------" >> "$LOGFILE"
 
 # Record timing
 start_time=$(date +%s)
 
 # Run your Python script
-python cvh_spin_1_half_test.py >> log.txt 2>&1
+python cvh_spin_1_half_test.py >> "$LOGFILE" 2>&1
 
 end_time=$(date +%s)
 wall_time=$((end_time - start_time))
 
-echo "----------------------------------------" >> log.txt
-echo "Job finished at $(date)" >> log.txt
-echo "Total execution time: ${wall_time} seconds" >> log.txt
+echo "----------------------------------------" >> "$LOGFILE"
+echo "Job finished at $(date)" >> "$LOGFILE"
+echo "Total execution time: ${wall_time} seconds" >> "$LOGFILE"
